8050c08f1fb1e80cb82477de2cca9a8bb1542c73
Rafael Martínez <it-rafamartinez@outlook.es>
2023-03-13 13:51:44 +0100

    ejercicios array

---
+++ 4_Array_Diseño_2_Capas_POO_Kotlin/4.0_Array_y_diseño_en_dos_capas/01_Arrays/Ejercicios/teoria/mios/Ejercicios_U4_B11/src/Ejercicio_U4_B11_E7.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio U4_B11_E7<p>
+ * Añade código al ejemplo anterior de forma que demuestre que si añado
+ * <pre>
+int[] a = {0,2,4,6,8};
+int[] b = {1,3,5,7,9};
+int[] c;
+c=a;
+a=b;
+ * </pre>
+ * la situación pasa a ser <code>c</code> apunta al primer array y <code>a</code> y <code>b</code> apuntan al segundo
+ * <p>
+ * Para demostrarlo imprime los Arrays a los que referencian <code>a</code>, <code>b</code> y <code>c</code>.
+ */
+public class Ejercicio_U4_B11_E7 {
+        
+}
