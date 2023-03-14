be437f10a8774cb1949cae89cb82962eb285bf0c
Rafael Martínez <it-rafamartinez@outlook.es>
2023-03-14 20:30:06 +0100

    add array exercises E6

--- 4_Array_Diseño_2_Capas_POO_Kotlin/4.0_Array_y_diseño_en_dos_capas/01_Arrays/Ejercicios/teoria/mios/Ejercicios_U4_B11/src/Ejercicio_U4_B11_E5.java
+++ 4_Array_Diseño_2_Capas_POO_Kotlin/4.0_Array_y_diseño_en_dos_capas/01_Arrays/Ejercicios/teoria/mios/Ejercicios_U4_B11/src/Ejercicio_U4_B11_E5.java
@@ -1,41 +1,48 @@
 /**
  * Ejercicio U4_B11_E5:<p>
  * Utilizando los operadores lógicos, imprime las tablas de verdad
- * de a and b, a or b , a exor b y a nand b.<p>
+ * de <code>a</code> and <code>b</code>, <code>a</code> or <code>b</code>,
+ * <code>a</code> exor <code>b</code> y <code>a</code> nand <code>b</code>.<p>
  *
  * Las operaciones anteriores tienen su correspondiente operador
  * java excepto nand que la implementamos manualmente como not and. <p>
  * Los valores de a y b deben de estar almacenados obligatoriamente
  * para este ejercicio en arrays de tipo boolean con el siguiente aspecto.
  * <pre>
- * a
- *    false    false    true    true
- *b
- *    false    true     false   true
- *
+a
+  false    false    true    true
+b
+  false    true     false   true
  * </pre>
  * Y la salida será (es suficiente con el and y el or):
  * <pre>
- *  L:\Programacion>java Unidad3
- * A       B       AorB    AandB    AexorB   AnandB
- * false   false   false   false    false    true
- * false   true    true    false    true     true
- * true    false   true    false    true     true
- * true    true    true    true     false    false
- *
+L:\Programacion>java Unidad3
+A       B       AorB    AandB    AexorB   AnandB
+false   false   false   false    false    true
+false   true    true    false    true     true
+true    false   true    false    true     true
+true    true    true    true     false    false
  * </pre>
  *
  */
-
 public class Ejercicio_U4_B11_E5 {
 
   public static void main(String[] args) {
     boolean[] a = { false, false, true, true };
     boolean[] b = { false, true, false, true };
-    System.out.println(" A      B       AorB    AandB    AexorB   AnandB");
+    System.out.println(" A      B      AorB   AandB  AxorB  AnandB");
     for (int i = 0; i < a.length; i++) {
-        
-      System.out.println(String.format(" %-5b |%-5b |",a[i], b[i] ));
+      System.out.println(
+        String.format(
+          " %-5b  %-5b  %-5b  %-5b  %-5b  %-5b",
+          a[i],
+          b[i],
+          (a[i] | b[i]),
+          (a[i] & b[i]),
+          (a[i] ^ b[i]),
+          (a[i] ? !b[i] : true)
+        )
+      );
     }
   }
 }
