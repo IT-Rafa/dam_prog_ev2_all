1dc7e6b358621facaa4f079c7c82186aeee0b948
Rafael Martínez <it-rafamartinez@outlook.es>
2023-03-18 16:22:21 +0100

    B11_E11

--- 4_Array_Diseño_2_Capas_POO_Kotlin/4.1_Array_y_diseño_en_dos_capas/01_Arrays/2_Ejercicios/1_EnTeoria/mios/Ejercicios_U4_B11/src/Ejercicio_U4_B11_E11.java
+++ 4_Array_Diseño_2_Capas_POO_Kotlin/4.1_Array_y_diseño_en_dos_capas/01_Arrays/2_Ejercicios/1_EnTeoria/mios/Ejercicios_U4_B11/src/Ejercicio_U4_B11_E11.java
@@ -1,11 +1,51 @@
 /**
  * Ejercicio U4_B11_E11<p>
  * Crea un array de 5 amigos de la clase Persona. 
+ * <pre>
+class Persona{
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+ * </pre>
  * Pide datos por teclado. Utiliza un bucle para recoger los datos 
  * y muestra los datos del array por pantalla.
  */
-public class Ejercicio_U4_B11_E11 {
-        public static void main(String[] args){
+import java.util.Scanner;
 
-        }
+public class Ejercicio_U4_B11_E11 {
+
+  public static void main(String[] args) {
+    Persona[] amigos = new Persona[5];
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Cliente " + i);
+
+      System.out.print("   Nombre: ");
+      String nombre = sc.nextLine();
+      System.out.print("   Edad: ");
+      int edad = Integer.parseInt(sc.nextLine());
+
+      amigos[i] = new Persona(nombre, edad);
+    }
+
+    for(Persona p: amigos){
+        System.out.println("Nombre: " + p.nombre + "; Edad: " + p.edad);
+    }
+  }
+}
+
+class Persona {
+
+  String nombre;
+  int edad;
+
+  public Persona(String nombre, int edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
 }
