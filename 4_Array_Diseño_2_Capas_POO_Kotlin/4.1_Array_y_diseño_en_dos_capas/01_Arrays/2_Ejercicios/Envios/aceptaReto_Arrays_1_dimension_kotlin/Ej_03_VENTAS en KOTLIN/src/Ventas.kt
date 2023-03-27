fd4f3affa99930fce066d53689aa6a657c60f88d
Rafael Martínez <it-rafamartinez@outlook.es>
2023-03-27 09:51:55 +0200

    arrays

--- 4_Array_Diseño_2_Capas_POO_Kotlin/4.1_Array_y_diseño_en_dos_capas/01_Arrays/2_Ejercicios/Envios/aceptaReto_Arrays_1_dimension_kotlin/Ej_03_VENTAS en KOTLIN/src/Ventas.kt
+++ 4_Array_Diseño_2_Capas_POO_Kotlin/4.1_Array_y_diseño_en_dos_capas/01_Arrays/2_Ejercicios/Envios/aceptaReto_Arrays_1_dimension_kotlin/Ej_03_VENTAS en KOTLIN/src/Ventas.kt
@@ -1,35 +1,43 @@
 fun main() {
         val totalDias = 6
+        val diasSemana =  listOf("LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO")
         val valoresDias = mutableListOf<Double>()
-        for(indices in 0 until totalDias){
+        for (indices in 0 until totalDias) {
             valoresDias.add(readln().toDouble())
         }
     
         var indiceMax = 0
         var indiceMin = 0
         var sumaDias = 0.0
-        for(index in valoresDias.indices){
+        for (index in valoresDias.indices) {
             sumaDias += valoresDias[index]
-            if(valoresDias[indiceMax] <= valoresDias[index] ){
+            if (valoresDias[indiceMax] <= valoresDias[index]) {
                 indiceMax = index
             }
-            if(valoresDias[indiceMin] >= valoresDias[index] ){
+            if (valoresDias[indiceMin] >= valoresDias[index]) {
                 indiceMin = index
             }
-        }
-        val media = sumaDias / totalDias
-        val domingo = valoresDias[totalDias-1]
-        val dia = when (indiceMax) {
-            0 -> "Martes"
-            1 -> "Miércoles"
-            2 -> "Jueves"
-            3 -> "Viernes"
-            4 -> "Sábado"
-            5 -> "Domingo"
-            else -> "No válido."
-        }
-        println(dia)
     
-        println("max: $indiceMax; min: $indiceMin; domingo superior media: $domingo / $media")
-    }
+        }
+        indiceMax++
+        indiceMin++
+        val media = sumaDias / totalDias
+        val domingo = valoresDias[totalDias - 1]
+        print(if (indiceMax == indiceMin) "EMPATE" else "${diasSemana[indiceMax]} ${diasSemana[indiceMin]}")
+        println(if (domingo >= media) " SI" else " NO")
+}
+    /*
+    -- ENTRADA
+    185.50
+    250.36
+    163.45
+    535.20
+    950.22
+    450.38
+    -- SALIDA
+    SABADO JUEVES SI
+    -- ENTRADA
+    -1
+    -- FIN
+    
     
