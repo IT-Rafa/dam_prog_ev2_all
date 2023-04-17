b20b72433e0f8909f743df1fcfd15fb27c9e14a0
Rafael Martínez <it-rafamartinez@outlook.es>
2023-04-17 22:44:11 +0200

    arboles acabado2

--- 3_evaluacion/6_Colecciones I._Estructuras_dinámicas/6.2_Estructuras_dinámicas/2_Ejercicios/Envios/Ej_04_AlturaArbolGeneral_num_310/src/Ej_04_AlturaArbolGeneral_num_310/Ej_04_AlturaArbolGeneral_num_310.java
+++ 3_evaluacion/6_Colecciones I._Estructuras_dinámicas/6.2_Estructuras_dinámicas/2_Ejercicios/Envios/Ej_04_AlturaArbolGeneral_num_310/src/Ej_04_AlturaArbolGeneral_num_310/Ej_04_AlturaArbolGeneral_num_310.java
@@ -92,31 +92,40 @@ public class Ej_04_AlturaArbolGeneral_num_310 {
    */
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
+    // Cant descripciones árboles a introducir
     int cant;
 
-    // Pedimos cantidad Árboles a introducir
+    // Pedimos cantidad descripciones Árboles a introducir
     while (true) {
       try {
         while (!sc.hasNext()) sc.next();
         cant = Integer.parseInt(sc.nextLine());
         if (cant > 0) {
+          // Si cantidad es mayor que 1, aceptamos
           break;
         } else {
+          // Cantidad menor que uno, pedimos de nuevo
           System.out.println("Cantidad menor que 1; Repite entrada");
         }
       } catch (NumberFormatException e) {
+        // Cantidad no válida, pedimos de nuevo
         System.out.println("Entrada no válida; Repite entrada");
       }
     }
 
-    // Repetimos por cantidad
+    // Repetimos por cantidad descripciones Árboles
     for (int i = 0; i < cant; i++) {
       // Pedimos descripción del árbol
       String input = sc.nextLine();
       // Dividimos string en array
 
+      // Creamos árbol
       Arbol arbol = new Arbol(input);
+
+      // Mostramos árbol (para comprobar)
       // arbol.imprimirArbol();
+
+      // Imprimimos la altura
       System.out.println(arbol.getAltura());
     }
 
@@ -125,19 +134,27 @@ public class Ej_04_AlturaArbolGeneral_num_310 {
 }
 
 /**
- * Organizador tipo arbol, en este caso Árbol Árbol n-ario
- * (Binary Search Tree)<p>
+ * Organizador tipo arbol, en este caso Árbol Árbol n-ario (n-ary tree)<p>
  * Una arbol, o tree, en inglés, es una colección de datos en los que
  * cada dato tiene enlazado varios enlaces a otros objetos.<p>
  *
- * En este caso cada nodo puede tener varios enlaces
+ * En este caso cada nodo puede tener entre 0 e infinitos enlaces
  *
  * En este caso está basada en clases autorreferenciadas
  */
-
 class Arbol {
 
+  /**
+   * Nodo de inicio del arbol
+   */
   private NodoArbol raiz;
+
+  /**
+   * Descripción de los nodos del árbol
+   * (primero los hijos de la raiz)
+   * (Para cada hijo, se indican sus hijos)
+   * (Al acabar los hijos, se sube el nivel y se crean los hermanos (si hay))
+   */
   private int[] datosArbol;
 
   /**
@@ -153,31 +170,56 @@ class Arbol {
     }
   }
 
+  /**
+   * Comprueba la altura de todos los hijos del Nodo en parámetro y
+   * devuelve la altura mayor de todos.
+   *
+   * @param nodo Nodo a comprobar
+   * @return altura del mas alto de los hijos del nodo
+   */
   private int getAltura(NodoArbol nodo) {
+    // Preparamos array para guardar las alturas de cada hijo
     int[] alturaHijos = new int[nodo.hijos.length];
+
+    // Recorremos hijos de forma secuencial
     for (int i = 0; i < nodo.hijos.length; i++) {
+      // El hijo ya es una altura (+1) y recurrimos por si también tiene hijos
       alturaHijos[i] += getAltura(nodo.hijos[i]) + 1;
     }
+
+    // Con los las alturas de cada hijo, buscamos la mayor
     int max = 0;
     for (int n : alturaHijos) {
       if (n > max) {
         max = n;
       }
     }
+    // Devolvemos la mayor
     return max;
   }
 
+  /**
+   * Constructor para el árbol con los datos en parámetro
+   *
+   * @param desc Descripción del árbol
+   */
   public Arbol(String desc) {
-    this.raiz = null;
-
+    // Convertimos descripción en array de int
     String[] datosSt = desc.split(" ");
+
+    // Creamos atributo y guardamos los datos como enteros
     datosArbol = new int[datosSt.length];
     for (int i = 0; i < datosSt.length; i++) {
       this.datosArbol[i] = Integer.parseInt(datosSt[i]);
     }
+
+    // Creamos arbol según la descripción
     crearArbol();
   }
 
+  /**
+   * Crea el árbol según la descripción (atributo datosArbol)
+   */
   private void crearArbol() {
     // 3 2 0 0 0 1 0
     // CREAMOS NODO RAIZ
@@ -190,6 +232,15 @@ class Arbol {
     crearHijos(this.raiz, 0);
   }
 
+  /**
+   * Crea cada hijo del nodo del parámetro , usando la posición de
+   * datosArbol donde se guardan la cantidad de hijos
+   *
+   * @param nodo Nodo en donde se añadirán los nodos hijos
+   * @param pos Índice del atributo datosArbol donde está la cantidad
+   *    de hijos guardada
+   * @return Nueva posición, tras crear los hijos
+   */
   private int crearHijos(NodoArbol nodo, int pos) {
     int cantHijosPadre = datosArbol[pos];
     for (int i = 0; i < cantHijosPadre; i++) {
