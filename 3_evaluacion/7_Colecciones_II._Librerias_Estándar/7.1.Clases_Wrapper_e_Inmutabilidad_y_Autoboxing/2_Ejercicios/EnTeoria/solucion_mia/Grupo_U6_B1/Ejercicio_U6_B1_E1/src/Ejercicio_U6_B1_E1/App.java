da3ea02f1c0240b6df28a3bbc6be7f084701b40b
Rafael Martínez Costas <it-rafamartinez@outlook.es>
2023-04-18 17:43:53 +0200

    Delete 3_evaluacion/7_Colecciones_II._Librerias_Estándar directory

--- 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.1.Clases_Wrapper_e_Inmutabilidad_y_Autoboxing/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U6_B1/Ejercicio_U6_B1_E1/src/Ejercicio_U6_B1_E1/App.java
+++
@@ -1,6 +0,0 @@
-package Ejercicio_U6_B1_E1;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.3.El_FrameWork_Collections /2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B4B/Ejercicio_U7_B4B_E1/src/Ejercicio_U7_B4B_E1/App.java
+++
@@ -1,6 +0,0 @@
-package Ejercicio_U7_B4B_E1;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
