237d11eabdb603384c42ea8c6e36c25b274fd6b0
Rafael Martínez <it-rafamartinez@outlook.es>
2023-04-23 21:21:15 +0200

    U7_B4_5

--- 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.3._El_FrameWork_Collections/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B4/Ejercicio_U7_B4_4/src/Ejercicio_U7_B4_4/package-info.java
+++ 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.3._El_FrameWork_Collections/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B4/Ejercicio_U7_B4_4/src/Ejercicio_U7_B4_4/package-info.java
@@ -12,14 +12,18 @@
  * a Ramiro el suyo le ha empezado a hacer cosas raras. 
  * En concreto, cuando pulsa ciertas teclas el teclado parece interpretar 
  * que se han pulsado otras.<p>
- * Después de un rato de análisis ha llegado a la conclusión de que la 
- * pulsación de la tecla del guión (-) tiene el mismo resultado que si 
- * pulsa la tecla Inicio, y el cursor se le va al principio de la línea. 
- * Algo parecido le ocurre con el +, que se lleva el cursor al final 
- * igual que la tecla Fin. Y al pulsar * se consigue el mismo efecto que 
- * con la tecla de la flecha derecha. Pero lo peor de todo es lo que 
- * ocurre con el 3: ¡hace lo mismo que la tecla Supr, que borra la letra 
- * que hay a la derecha del cursor!<p>
+ * Después de un rato de análisis ha llegado a la conclusión de que:
+ * <ul>
+ *  <li>la pulsación de la tecla del guión (-) tiene el mismo resultado que si 
+ *  pulsa la tecla Inicio, y el cursor se le va al principio de la línea.</li>
+ *  <li> Algo parecido le ocurre con el +, que se lleva el cursor al final 
+ *  igual que la tecla Fin.</li>
+ *  <li>Y al pulsar * se consigue el mismo efecto que con la tecla de la flecha 
+ *  derecha.</li>
+ *  <li>Pero lo peor de todo es lo que ocurre con el 3: ¡hace lo mismo que la tecla Supr, que borra la letra 
+ *  que hay a la derecha del cursor!</li>
+ * </ul>
+ * <p>
  * El resultado es que cuando Ramiro se pone a copiar un texto sin mirar 
  * la pantalla, el resultado final de lo que ha escrito puede terminar 
  * siendo muy distinto de lo que quería escribir. ¿Qué texto saldrá como 
@@ -38,6 +42,7 @@ EDA
 EDA-333
 dD-3*A-E+
 EDA-3E*3A
+E DA
  * </pre>
  * <h4>Salida de ejemplo</h4>
  * <pre>
@@ -45,6 +50,28 @@ EDA
 
 EDA
 EDA
+E DA
  * </pre>
  */
 package Ejercicio_U7_B4_4;
+/**
+ * - = inicio
+ * + = fin
+ * * = flecha derecha
+ * 3 = sup
+ *
+ * PASOS---
+ *
+ * Entrada: dD-3*A-E+
+ *
+ * d        d           pos 1
+ * D        dD          pos 2
+ * -        dD          pos 0
+ * 3        D           pos 0
+ * *        D           pos 1
+ * A        DA          pos 2
+ * -        DA          pos 0
+ * E        EDA         pos 1
+ * +        EDA         pos 2
+ *
+ */
--- 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.3._El_FrameWork_Collections/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B4/Ejercicio_U7_B4_5/src/Ejercicio_U7_B4_5/package-info.java
+++ 3_evaluacion/7_Colecciones_II._Librerias_Estándar/7.3._El_FrameWork_Collections/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B4/Ejercicio_U7_B4_5/src/Ejercicio_U7_B4_5/package-info.java
@@ -1,6 +1,27 @@
 /**
  * Lista clases para ejercicio:<p>
  * <h3>Ejercicio U7_B4_5:</h3><p>
+ * En el boletín de estructuras dinámicas escribiste una clase 
+ * Pila “a pelo” usando nodos enlazados. <p>
+ * Escribe ahora una clase Pila pero utilizando subyacentemente
+ * LinKedList
+ * <pre>
+import java.util.LinkedList;
 
+class Pila<T>{
+    LinkedList<T> listaDatos= new LinkedList<>();
+    //escribir push(), pop() y esVacia()
+}
+
+public class App {
+    public static void main(String args[]) {
+        Pila<Character> miPila= new Pila<>();
+        miPila.push('a');miPila.push('b');miPila.push('c');
+        while(!miPila.esVacia()){
+            System.out.print(miPila.pop()+" ");
+        }   
+    }
+}
+ * </pre>  
  */
 package Ejercicio_U7_B4_5;
