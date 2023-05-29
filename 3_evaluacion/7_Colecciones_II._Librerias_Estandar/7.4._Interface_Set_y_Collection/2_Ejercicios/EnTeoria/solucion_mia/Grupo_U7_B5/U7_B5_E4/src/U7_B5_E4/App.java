eb6cf394bfd96a069566a78189ed0bf22f15db9f
it-ra <it-rafamartinez@outlook.es>
2023-05-29 19:26:46 +0200

    Interface Set

--- 3_evaluacion/7_Colecciones_II._Librerias_Estandar/7.4._Interface_Set_y_Collection/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B5/U7_B5_E4/src/U7_B5_E4/App.java
+++ 3_evaluacion/7_Colecciones_II._Librerias_Estandar/7.4._Interface_Set_y_Collection/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B5/U7_B5_E4/src/U7_B5_E4/App.java
@@ -1,76 +1,74 @@
 package U7_B5_E4;
 
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Ejercicio U7_B5_E4:<p>
- * Quizá las operaciones matemáticas más típicas de conjuntos son: <p>
- * union, diferencia e intersección. <p>
- * Haz un programa que prueba con los datos de los conjuntos A y B las 
- * tres operaciones indicadas. <p>
- * El resultado del programa debe ser el siguiente. <p>
- * Union = Juntar ambos grupos<p>
- * Diferencia = Eliminar de un grupo los elementos que están en el otro<p>
- * Intersección = Quedarse solo con los elementos que también tiene el otro grupo<p>
-<pre>
+ * Ejercicio U7_B5_E4:
+ * <p>
+ * Quizá las operaciones matemáticas más típicas de conjuntos son:
+ * <p>
+ * union, diferencia e intersección.
+ * <p>
+ * Haz un programa que prueba con los datos de los conjuntos A y B las
+ * tres operaciones indicadas.
+ * <p>
+ * El resultado del programa debe ser el siguiente.
+ * <p>
+ * Union = Juntar ambos grupos
+ * <p>
+ * Diferencia = Eliminar de un grupo los elementos que están en el otro
+ * <p>
+ * Intersección = Quedarse solo con los elementos que también tiene el otro
+ * grupo
+ * <p>
+ * 
+ * <pre>
 run:
 A:[5, 7, 9, 19]
 B:[5, 7, 10, 20]
 A union B: [5, 7, 9, 10, 19, 20]
 A diferencia B: [9, 19]
 A intersección B: [5, 7]
-</pre>
- * Consulta el API para saber los métodos que debes usar para hacer las 
- * operaciones deseadas. Usa TreeSet ya que al ver los elementos ordenados 
+ * </pre>
+ * 
+ * Consulta el API para saber los métodos que debes usar para hacer las
+ * operaciones deseadas. Usa TreeSet ya que al ver los elementos ordenados
  * es más fácil comprobar que las operaciones son OK.
  */
 public class App {
 
-  /**
-   * Usamos métodos para poner los conjuntos con los valores originales<p>
-   * Mostramos los conjuntos originales<p>
-   * Para cada operación<p>
-   * <ul>
-   *    <li> Hacemos la operación y mostramos resultado con la variable
-   *        que lo llamo, A</li>
-   *    <li> Como la variable cambió, la volvemos a dejar como estaba,
-   *        con el método inicial</li>
-   * </ul>
-   */
-  public static void main(String[] args) {
-    TreeSet<Integer> elementosA = restore_A();
-    TreeSet<Integer> elementosB = restore_B();
+    /**
+     * Usamos métodos para poner los conjuntos con los valores originales
+     * <p>
+     * Mostramos los conjuntos originales
+     * <p>
+     * Para cada operación
+     * <p>
+     * <ul>
+     * <li>Hacemos la operación y mostramos resultado con la variable
+     * que lo llamo, A</li>
+     * <li>Como la variable cambió, la volvemos a dejar como estaba,
+     * con el método inicial</li>
+     * </ul>
+     */
+    public static void main(String[] args) {
+        Set<Integer> elementosA = Set.of(5, 7, 9, 19);
+        Set<Integer> elementosB = Set.of(5, 7, 10, 20);
 
-    System.out.println("A:" + elementosA);
-    System.out.println("B:" + elementosB);
+        System.out.println("A:" + elementosA);
+        System.out.println("B:" + elementosB);
 
-    elementosA.addAll(elementosB);
-    System.out.println("A union B:" + elementosA);
-    elementosA = restore_A();
+        Set<Integer> operacion = new TreeSet<>(elementosA);
+        operacion.addAll(elementosB);
+        System.out.println("A union B:" + operacion);
 
-    elementosA.removeAll(elementosB);
-    System.out.println("A diferencia B:" + elementosA);
-    elementosA = restore_A();
+        operacion = new TreeSet<>(elementosA);
+        operacion.removeAll(elementosB);
+        System.out.println("A diferencia B:" + operacion);
 
-    elementosA.retainAll(elementosB);
-    System.out.println("A intersección B:" + elementosA);
-  }
-
-  private static TreeSet<Integer> restore_A() {
-    TreeSet<Integer> result = new TreeSet<>();
-    result.add(5);
-    result.add(7);
-    result.add(9);
-    result.add(19);
-    return result;
-  }
-
-  private static TreeSet<Integer> restore_B() {
-    TreeSet<Integer> result = new TreeSet<>();
-    result.add(5);
-    result.add(7);
-    result.add(10);
-    result.add(20);
-    return result;
-  }
+        operacion = new TreeSet<>(elementosA);
+        operacion.retainAll(elementosB);
+        System.out.println("A intersección B:" + operacion);
+    }
 }
--- 3_evaluacion/7_Colecciones_II._Librerias_Estandar/7.4._Interface_Set_y_Collection/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B5/U7_B5_E5/src/U7_B5_E5/App.java
+++ 3_evaluacion/7_Colecciones_II._Librerias_Estandar/7.4._Interface_Set_y_Collection/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U7_B5/U7_B5_E5/src/U7_B5_E5/App.java
@@ -1,13 +1,48 @@
 package U7_B5_E5;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
 /**
- * Ejercicio U7_B5_E5:<p>
+ * Ejercicio U7_B5_E5:
+ * <p>
  * Reto Michael J. Fox y el Pato Donald categoría conjuntos.
  * Utiliza obligatoriamente conjuntos para resolverlo.
  */
 public class App {
+    /**
+     * Pide cantidad cumpleaños
+     * Si 0 finaliza
+     * Pide los cumpleaños dia/mes/año
+     * Si alguno está repetido muestra SÍ, si no, muestra NO
+     * 
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (!sc.nextLine().startsWith("0")) {
+            LinkedHashSet<String> listaCumpleaños = new LinkedHashSet<>();
+            boolean repetido = false;
+            String cumples = sc.nextLine();
+            String[] listaCumple = cumples.split(" ");
+            for (String c : listaCumple) {
+                String[] cumpleSt = c.split("/");
+                Integer[] cumple = new Integer[2];
 
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+                cumple[0] = Integer.parseInt(cumpleSt[0]);
+                cumple[1] = Integer.parseInt(cumpleSt[1]);
+
+                if (!listaCumpleaños.add(cumple[0] + "/" + cumple[1])) {
+                    repetido = true;
+                    break;
+                }
+
+            }
+            if (repetido) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
 }
