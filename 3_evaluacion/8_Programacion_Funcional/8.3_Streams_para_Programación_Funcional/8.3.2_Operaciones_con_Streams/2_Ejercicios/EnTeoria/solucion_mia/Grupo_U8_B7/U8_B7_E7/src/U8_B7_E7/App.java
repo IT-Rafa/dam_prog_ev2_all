531062ae0ef12c5d2ff3bf5646532d4b98bb5831
it-ra <it-rafamartinez@outlook.es>
2023-06-15 19:37:20 +0200

    U8_B7_E7

--- 3_evaluacion/8_Programacion_Funcional/8.3_Streams_para_Programación_Funcional/8.3.2_Operaciones_con_Streams/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B7/U8_B7_E7/src/U8_B7_E7/App.java
+++ 3_evaluacion/8_Programacion_Funcional/8.3_Streams_para_Programación_Funcional/8.3.2_Operaciones_con_Streams/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B7/U8_B7_E7/src/U8_B7_E7/App.java
@@ -3,27 +3,28 @@ package U8_B7_E7;
 import java.util.Scanner;
 
 /**
- * Clase con ejecutable de Ejercicio U8_B7_E7<p>
+ * Clase con ejecutable de Ejercicio U8_B7_E7
+ * <p>
  */
 public class App {
 
-  /**
-   * Ejecutable Ejercicio U8_B7_E7<p>
-   * Resumen enunciado PDF
-   * <p>
-   * Introducir 1 número (positivo y menor de 1000) ¡
-   * Si el número es 0, finaliza programa
-   * El resto mostrará su valor en base 1 (un 1 por la cantidad: 1=1; 5=11111)
-   * La entrada estará compuesta por distintos números mayores que 0 y
-   * que nunca serán mayores que 1.000,
-   *
-   * <p>
-   * Prohibido usar if ni bucle
-   */
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
+    /**
+     * Ejecutable Ejercicio U8_B7_E7
+     * <p>
+     * Resumen enunciado PDF
+     * <p>
+     * Introducir 1 número (positivo y menor de 1000) ¡
+     * Si el número es 0, finaliza programa
+     * El resto mostrará su valor en base 1 (un 1 por la cantidad: 1=1; 5=11111)
+     * La entrada estará compuesta por distintos números mayores que 0 y
+     * que nunca serán mayores que 1.000,
+     * <p>
+     * Prohibido usar if ni bucle
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
-    sc.close();
-  }
+        sc.close();
+    }
 }
