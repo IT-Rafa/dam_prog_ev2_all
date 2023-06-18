fbbf104537d48c16970fbb9d632f2e0240c83eff
Rafael Martínez <it-rafamartinez@outlook.es>
2023-06-18 21:24:52 +0200

    U8_B7_E7

--- 3_evaluacion/8_Programacion_Funcional/8.3_Streams_para_Programación_Funcional/8.3.2_Operaciones_con_Streams/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B7/U8_B7_E7/src/U8_B7_E7/App.java
+++ 3_evaluacion/8_Programacion_Funcional/8.3_Streams_para_Programación_Funcional/8.3.2_Operaciones_con_Streams/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B7/U8_B7_E7/src/U8_B7_E7/App.java
@@ -1,6 +1,10 @@
 package U8_B7_E7;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Clase con ejecutable de Ejercicio U8_B7_E7
@@ -8,23 +12,27 @@ import java.util.Scanner;
  */
 public class App {
 
-    /**
-     * Ejecutable Ejercicio U8_B7_E7
-     * <p>
-     * Resumen enunciado PDF
-     * <p>
-     * Introducir 1 número (positivo y menor de 1000) ¡
-     * Si el número es 0, finaliza programa
-     * El resto mostrará su valor en base 1 (un 1 por la cantidad: 1=1; 5=11111)
-     * La entrada estará compuesta por distintos números mayores que 0 y
-     * que nunca serán mayores que 1.000,
-     * <p>
-     * Prohibido usar if ni bucle
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+  /**
+   * Ejecutable Ejercicio U8_B7_E7<p>
+   * Resumen enunciado PDF
+   * <p>
+   * Introducir 1 número (positivo y menor de 1000) ¡
+   * Si el número es 0, finaliza programa
+   * El resto mostrará su valor en base 1 (un 1 por la cantidad: 1=1; 5=11111)
+   * La entrada estará compuesta por distintos números mayores que 0 y
+   * que nunca serán mayores que 1.000,
+   *
+   * <p>
+   * Prohibido usar if ni bucle
+   */
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        sc.close();
-    }
+    Stream
+      .generate(sc::nextInt)
+      .takeWhile(n -> n > 0)
+      .forEach(System.out::println);
+
+    sc.close();
+  }
 }
