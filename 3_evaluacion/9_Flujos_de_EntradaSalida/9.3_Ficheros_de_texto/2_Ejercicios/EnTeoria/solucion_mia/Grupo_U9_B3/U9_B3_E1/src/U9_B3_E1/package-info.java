9880a3c968bec2d157dc3c32b09971b742854c15
Rafael Martínez <it-rafamartinez@outlook.es>
2023-06-21 21:43:32 +0200

    ficheros texto

---
+++ 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/package-info.java
@@ -0,0 +1,37 @@
+/**
+ * package con clases para:
+ * <h3>Ejercicio U9_B3_E1:</h3>
+ * Escribe en App, código de forma que args[0] sea un 
+ * fichero xml que puede contener con <code>\n</code> y 
+ * <code>\t</code> y crea un fichero de salida xml con 
+ * el nombre indicado en args[1] sin <code>\n</code> ni 
+ * <code>\t</code>.
+ * <p>
+ * Intenta resolver el problema a la manera tradicional 
+ * y también con estilo programación funcional.
+ * <p>
+ * Puedes hacer un xml en un fichero de texto por ejemplo 
+ * con:
+ * <pre>
+{@code
+<agenda>
+    <persona>
+        <nombre>Elías</nombre>
+        <apellido>Lozano</apellido>
+        <edad>7</edad>
+    </persona>
+    <persona>
+        <nombre>Román</nombre>
+        <apellido>López</apellido>
+        <edad>6</edad>
+    </persona>
+    <persona>
+        <nombre>Telma</nombre>
+        <apellido>Lozano López</apellido>
+        <edad>3</edad>
+    </persona>
+</agenda>
+}
+ * </pre>
+ */
+package U9_B3_E1;
---
+++ 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/progFuncional/App.java
@@ -0,0 +1,33 @@
+package U9_B3_E1.progFuncional;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Clase con ejecutable de Ejercicio U9_B3_E1<p>
+ */
+public class App {
+
+  /**
+   * Ejecutable Ejercicio U9_B3_E1<p>
+   */
+  public static void main(String[] args) {
+    // ruta al proyecto del ejercicio
+    String path =
+      "9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/";
+    try (
+      FileWriter ficheroEscritura = new FileWriter(
+        path + "pruebaPrintWriter.txt"
+      );
+      PrintWriter pw = new PrintWriter(ficheroEscritura);
+    ) {
+      for (int i = 0; i < 10; i++) {
+        pw.println("Linea " + i);
+      }
+      pw.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
