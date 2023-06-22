750c092af85d1a9b5d046607b9c9cf1954d09963
Rafael Martínez <it-rafamartinez@outlook.es>
2023-06-22 15:13:32 +0200

    texto done

--- 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/progFuncional/App.java
+++ 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/progFuncional/App.java
@@ -1,11 +1,22 @@
 package U9_B3_E1.progFuncional;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Clase con ejecutable de Ejercicio U9_B3_E1<p>
+ * Versión funcional.
+ * App con dos parámetros:
+ * <ul>
+ *  <li>Archivo Lectura tipo xml: Con \n y \t</li>
+ *  <li>Archivo Escritura tipo txt. Eliminando \n y \t</li>
+ * </ul>
+ * Por simpleza, ponemos el archivo lectura en la carpeta del ejercicio
+ * y el archivo de escritura en misma carpeta con nombres distintos
+ * para tradicional y funcional
  */
 public class App {
 
@@ -16,16 +27,32 @@ public class App {
     // ruta al proyecto del ejercicio
     String path =
       "9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/";
-    try (
-      FileWriter ficheroEscritura = new FileWriter(
-        path + "pruebaPrintWriter.txt"
-      );
-      PrintWriter pw = new PrintWriter(ficheroEscritura);
+
+    // Nombre del archivo xml a leer
+    String nombreLectFile = path + "Agenda.xml";
+    // Nombre del archivo txt a escribir
+    String nombreEscrFile = path + "escrituraFunc.txt";
+
+    try ( // Definimos archivo a leer
+      FileReader lectFile = new FileReader(nombreLectFile);
+      // Creamos lector con buffer de este archivo
+      BufferedReader flujoLec = new BufferedReader(lectFile);
+      // Definimos archivo a escribir
+      FileWriter escrFile = new FileWriter(nombreEscrFile);
+      // Creamos escritor con buffer de este archivo
+      BufferedWriter flujoEsc = new BufferedWriter(escrFile);
     ) {
-      for (int i = 0; i < 10; i++) {
-        pw.println("Linea " + i);
-      }
-      pw.close();
+      flujoLec
+        .lines()
+        .map(s -> s.replaceAll("\t", ""))
+        .map(s -> s.replaceAll("\n", ""))
+        .forEach(s -> {
+          try {
+            flujoEsc.write(s);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        });
     } catch (IOException e) {
       e.printStackTrace();
     }
--- 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/tradicional/App.java
+++ 3_evaluacion/9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/tradicional/App.java
@@ -1,11 +1,22 @@
 package U9_B3_E1.tradicional;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Clase con ejecutable de Ejercicio U9_B3_E1<p>
+ * Versión tradicional.
+ * App con dos parámetros:
+ * <ul>
+ *  <li>Archivo Lectura tipo xml: Con \n y \t</li>
+ *  <li>Archivo Escritura tipo txt. Eliminando \n y \t</li>
+ * </ul>
+ * Por simpleza, ponemos el archivo lectura en la carpeta del ejercicio
+ * y el archivo de escritura en misma carpeta con nombres distintos
+ * para tradicional y funcional
  */
 public class App {
 
@@ -16,16 +27,32 @@ public class App {
     // ruta al proyecto del ejercicio
     String path =
       "9_Flujos_de_EntradaSalida/9.3_Ficheros_de_texto/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U9_B3/U9_B3_E1/src/U9_B3_E1/";
+
+    // Nombre del archivo xml a leer
+    String nombreLectFile = path + "Agenda.xml";
+    // Nombre del archivo txt a escribir
+    String nombreEscrFile = path + "escrituraTrad.txt";
+
     try (
-      FileWriter ficheroEscritura = new FileWriter(
-        path + "pruebaPrintWriter.txt"
-      );
-      PrintWriter pw = new PrintWriter(ficheroEscritura);
+      // Definimos archivo a leer
+      FileReader lectFile = new FileReader(nombreLectFile);
+      // Creamos lector con buffer de este archivo
+      BufferedReader flujoLec = new BufferedReader(lectFile);
+      // Definimos archivo a escribir
+      FileWriter escrFile = new FileWriter(nombreEscrFile);
+      // Creamos escritor con buffer de este archivo
+      BufferedWriter flujoEsc = new BufferedWriter(escrFile);
     ) {
-      for (int i = 0; i < 10; i++) {
-        pw.println("Linea " + i);
+      // Preparamos espacio para guardar el byte
+      int data;
+      // Recorremos con buffer los bytes del archivo lectura
+      while ((data = flujoLec.read()) != -1) {
+        // Si no son \n o \t ( y í para comprobar codificación)
+        if (data != '\n' && data != '\t' && data != 'í') {
+          // Escribimos el byte en archivo lectura con buffer
+          flujoEsc.write(data);
+        }
       }
-      pw.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
