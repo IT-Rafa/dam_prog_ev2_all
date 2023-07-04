e325981c2ff1c540920054f11802081ad47536e5
Rafael Martínez <it-rafamartinez@outlook.es>
2023-07-04 19:00:10 +0200

    zip

--- 3_evaluacion/9_Flujos_de_EntradaSalida/9.4_las_clases_File_y_Files/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B4/U8_B4_E5/src/U8_B4_E5/App.java
+++ 3_evaluacion/9_Flujos_de_EntradaSalida/9.4_las_clases_File_y_Files/2_Ejercicios/EnTeoria/solucion_mia/Grupo_U8_B4/U8_B4_E5/src/U8_B4_E5/App.java
@@ -1,5 +1,6 @@
 package U8_B4_E5;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.zip.ZipFile;
 
@@ -13,14 +14,24 @@ public class App {
    * @throws IOException
    */
   public static void main(String[] args) {
-    try (ZipFile zipFile = new ZipFile("mizip.zip")) {
-      zipFile
-        .stream()
-        .sorted((zpe1, zpe2) -> (zpe1.getSize() < zpe2.getSize()) ? 1 : -1)
-        .forEach(zpe -> System.out.println(zpe.getName() + " " + zpe.getSize())
-        );
-    } catch (Exception e) {
-      System.out.println(e.getLocalizedMessage());
+    // Creamos archivo en el directorio:
+    // /run/media/it-ra/Datos/source/dam/prog2/dam_prog_ev2_all/3_evaluacion/miZip.zip
+    File f = new File("miZip.zip");
+    if (f.exists()) {
+      try (ZipFile zipFile = new ZipFile(f)) {
+        zipFile
+          .stream() // Creamos Stream de ZipEntry (representa cada archivo comprimido)
+          // Ordenamos por tamaño del archivo
+          .sorted((zpe1, zpe2) -> (zpe1.getSize() < zpe2.getSize()) ? 1 : -1)
+          // Mostramos los nombres y el tamaño de cada entrada
+          .forEach(zpe ->
+            System.out.println(zpe.getName() + " " + zpe.getSize())
+          );
+      } catch (Exception e) {
+        System.out.println(e.getLocalizedMessage());
+      }
+    } else {
+      System.out.println("Fichero no existe");
     }
   }
 }
